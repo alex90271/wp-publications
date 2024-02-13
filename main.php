a9244596dea14eb8f156b27b2d8fbc4ac50a789f
alex.alder <alex.alder@noaa.gov>
2024-02-13 10:35:58 -0500

    move stylesheets

--- main.php
+++ main.php
@@ -6,9 +6,13 @@ Version: 0.2.6.2024
 Author: UFS
 */
 
+function wpap_required_scripts()
+{
+    wp_enqueue_style('wpap-css', plugins_url('/css/stylesheet.css', __FILE__));
+    wp_enqueue_script('search-wpap-js', plugins_url('/js/search.js', __FILE__));
+}
+wpap_required_scripts();
 
-wp_register_script('search-wpap-js', plugins_url('/js/search.js', __FILE__), array('jquery'));
-wp_enqueue_script('search-wpap-js');
 function wpap_scripts()
 {
     wp_enqueue_media();
@@ -46,10 +50,10 @@ require_once('wpap-publication.php');
 
 add_filter('upload_mimes', 'wpap_add_bib_to_mimes');
 add_filter('manage_edit-publication_columns', 'wpap_show_publication_column');
-add_filter( 'wp_sprintf', function( $fragment ) {
-	$fragment = ( '%z' === $fragment ) ? '' : $fragment;
-	return $fragment;
-} );
+add_filter('wp_sprintf', function ($fragment) {
+    $fragment = ('%z' === $fragment) ? '' : $fragment;
+    return $fragment;
+});
 
 add_action('save_post', 'wpap_save_option_meta');
 add_action('init', 'wpap_create_publication');
@@ -57,6 +61,7 @@ add_action('manage_posts_custom_column', 'wpap_publication_custom_columns');
 add_action('add_meta_boxes', 'wpap_add_publication_options');
 
 add_shortcode('academicpubs', 'wpap_shortcode');
+add_shortcode('academicpubs-search', 'wpap_search_shortcode');
 
 add_action('plugins_loaded', 'wpap_loadtextdomain');
 
